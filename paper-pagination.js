e46d1386729f5c628e48bd8924e8bee0c1a5721c
Benoît Claveau <benoit.claveau@izzili.com>
2018-11-16 17:07:29 +0100

    Use LitElement

--- paper-pagination.js
+++ paper-pagination.js
@@ -1,194 +1,184 @@
-import { PolymerElement, html } from '@polymer/polymer';
-import '@polymer/paper-listbox/paper-listbox.js';
-import '@polymer/paper-item/paper-item.js';
+import { LitElement, html } from '@polymer/lit-element';
+import "@polymer/paper-button/paper-button.js";
 import '@polymer/paper-icon-button/paper-icon-button.js';
+import { classMap } from 'lit-html/directives/classMap';
 
-class PaperPagination extends PolymerElement {
+
+const styles = html`
+<style>
+    .container {
+        display: flex;
+        flex-direction: row;
+        justify-content:center;
+        align-items: center;
+    }
+    paper-listbox {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+    }
+    paper-button {
+        min-width: 0;
+        font-weight: var(--paper-pagination-active-weight, 400);
+        font-size: var(--paper-pagination-font-size, 13px);
+    }
+    .active {
+        color: var(--paper-pagination-active-color, #af4477);
+        font-weight: var(--paper-pagination-active-font-weight, 600);
+    }
+</style>
+`;
+
+class PaperPagination extends LitElement {
 
     static get properties() {
         return {
-            pageSize: Number,
-            pageCount: {
-                type: Number,
-                reflectToAttribute: true,
-                notify: true,
-                value() {
-                    return 1;
-                }
-            },
-            total: Number,
-            pages: {
-                type: Array,
-                value: []
-            },
-            rangeSize: {
-                type: Number,
-                reflectToAttribute: true,
-                notify: true,
-                value() {
-                    return 10;
-                }
-            },
-            currentPage: {
-                type: Number,
-                reflectToAttribute: true,
-                notify: true,
-                value() {
-                    return 0;
-                }
-            },
-            hidden: {
-                type: Boolean,
-                notify: true
-            },
-            hiddenFirstPageButton: Boolean,
-            hiddenLastPageButton: Boolean,
-            hiddenNextPageButton: Boolean,
-            hiddenPreviousPageButton: Boolean
+            itemsCount: Number,
+            itemsPerPage: Number,
+            range: Number,
+            index: Number
         }
     }
 
-    static get observers() {
-        return [
-            'currentPageChanged(currentPage)',
-            'rangeSizeChanged(rangeSize)',
-            'pagesChanged(pageSize, total)'
-        ]
+    get pageCount() {
+        return Math.ceil(this.itemsCount / this.itemsPerPage);
     }
 
-    ready() {
-        super.ready();
-        this.itemWidth = 50;
-        this.prevWidth = this.nextWidth = 40;
+    select(e, item) {
+        this.dispatchEvent(new CustomEvent('page-changed', { 
+            bubbles: true, 
+            composed: true, 
+            detail: {
+                page: item.page
+            }
+        }));
     }
 
     prev(e) {
-        e.preventDefault();
-        e.stopPropagation();
-        this.set("currentPage", Math.max(this.currentPage - 1, 0));
+        this.dispatchEvent(new CustomEvent('page-changed', { 
+            bubbles: true, 
+            composed: true, 
+            detail: {
+                page: Math.max(this.index - 1, 0)
+            }
+        }));
     }
 
     next(e) {
-        e.preventDefault();
-        e.stopPropagation();
-        this.set("currentPage", Math.min(this.currentPage + 1, this.pageCount - 1));
+        this.dispatchEvent(new CustomEvent('page-changed', { 
+            bubbles: true, 
+            composed: true, 
+            detail: {
+                page: Math.min(this.index + 1, this.pageCount - 1)
+            }
+        }));
     }
 
     first(e) {
-        e.preventDefault();
-        e.stopPropagation();
-        this.set("currentPage", 0);
+        this.dispatchEvent(new CustomEvent('page-changed', { 
+            bubbles: true, 
+            composed: true, 
+            detail: {
+                page: 0
+            }
+        }));
     }
 
     last(e) {
-        e.preventDefault();
-        e.stopPropagation();
-        this.set("currentPage", this.pageCount - 1);
+        this.dispatchEvent(new CustomEvent('page-changed', { 
+            bubbles: true, 
+            composed: true, 
+            detail: {
+                page: this.pageCount - 1
+            }
+        }));
     }
 
-    hiddenPagination(pages) {
-        this.set("hidden", pages.length <= 1);
-        return this.hidden;
-    }
-
-    pagesChanged(size, total) {
-        this.set("pageCount", Math.ceil(total / size));
-        var result = [];
-        for (var i = 0; i < this.pageCount; i++) {
-            result.push({
-                page: i,
-                label: i + 1,
-                hidden: true
-            });
-        }
-
-        this.set("pages", result);
-        this.currentPageChanged(this.currentPage);
-    }
-
-    currentPageChanged(currentPage) {
-        this.$.prev.disabled = currentPage == 0;
-        this.$.next.disabled = currentPage == this.pageCount - 1;
-
-        var rangeSizeIndex = this.rangeSize - 1;
+    getVisibleItems() {
+        const pageCount = this.pageCount;
+        var rangeSizeIndex = this.range - 1;
         var start = 0;
-        var end = Math.min(rangeSizeIndex, this.pageCount);
+        var end = Math.min(rangeSizeIndex, pageCount);
         var mid = Math.floor(rangeSizeIndex / 2);
+        
 
-        if (currentPage > mid) {
-            start = currentPage - mid;
+        if (this.index > mid) {
+            start = this.index - mid;
             end = start + rangeSizeIndex;
         }
 
-        if (currentPage > this.pageCount - mid - 1) {
-            end = this.pageCount - 1;
+        if (this.index > pageCount - mid - 1) {
+            end = pageCount - 1;
             start = end - rangeSizeIndex;
         }
 
-        for (var i = 0; i < this.pages.length; i++) {
-            this.set("pages." + i + ".hidden", i < start || i > end);
+        const res = [];
+        for (var i = start; i <= end; i++) {
+            res.push({
+                page: i,
+                label: i + 1,
+            })
         }
-
-        var myEffect = new KeyframeEffect(document.body, [], { duration: 350, easing: "ease-out" });
-        var scrollTop = document.body.scrollTop;
-
-        myEffect.onsample = function (timeFraction, effect, animation) {
-            timeFraction = timeFraction == null ? 1 : timeFraction;
-            effect.target.scrollTop = scrollTop - (scrollTop * timeFraction);
-        };
-        document.timeline.play(myEffect);
+        return res;
     }
 
-    rangeSizeChanged(rangeSize) {
-        this.currentPageChanged(this.currentPage);
-    }
-
-    onResize() {
-        var container = this.shadowRoot.querySelector(".paper-pagination-container")
-        var rect = container.getBoundingClientRect();
-        if (rect.top == 0 && rect.bottom == 0) return;
-        var width = rect.right - rect.left;
-        visible = this.pages.filter(function (item) {
-            return !item.hidden;
-        }).length;
-
-        var items = Math.floor((width - 40 - (this.prevWidth + this.nextWidth)) / this.itemWidth); //40 for bonus space
-
-        if (visible > items) {
-            this.previousRangeSize = this.rangeSize;
-            this.set("rangeSize", items);
-        }
-        else if (this.previousRangeSize && visible < items) {
-            var previousRangeSize = this.previousRangeSize;
-            delete this.previousRangeSize;
-            this.set("rangeSize", previousRangeSize);
-        }
-    }
-
-    static get template() {
+    render() {
+        const items = this.getVisibleItems();
         return html`
-<style>
-    :host {
-        --paper-listbox-background-color: transparent;
-    }
-    :host [hidden] {
-        display: none;
-    }
-    :host [pointer] {
-        cursor: pointer;
-    }
-</style>
-<div hidden$="[[hiddenPagination(pages)]]" class="paper-pagination-container" style="display:flex;flex-direction:row;justify-content:center;align-items: center;">
-    <paper-icon-button id="first" on-tap="first" icon="first-page" hidden$="[[hiddenFirstPageButton]]"></paper-icon-button>
-    <paper-icon-button id="prev" on-tap="prev" icon="chevron-left" hidden$="[[hiddenPreviousPageButton]]"></paper-icon-button>
-    <paper-listbox id="pages" selected="{{currentPage}}" selected-attribute="page" style="display:flex;flex-direction:row;justify-content:center;align-items: center;">
-        <template is="dom-repeat" items="[[pages]]">
-            <paper-item class="paper-pagination" hidden$="[[item.hidden]]" page="[[item.page]]" pointer>[[item.label]]</paper-item>
-        </template>
-    </paper-listbox>
-    <paper-icon-button id="next" on-tap="next" icon="chevron-right" hidden$="[[hiddenNextPageButton]]"></paper-icon-button>
-    <paper-icon-button id="last" on-tap="last" icon="last-page" hidden$="[[hiddenFirstPageButton]]"></paper-icon-button>
-</div>`;
+            ${styles}
+            <div class="container">
+                ${ !this.hiddenFirstPageButton &&
+                    html`
+                        <paper-icon-button 
+                            id="first" 
+                            @tap="${this.first}"
+                            icon="first-page" 
+                        ></paper-icon-button>
+                    `
+                }
+                ${ !this.hiddenPreviousPageButton &&
+                    html`
+                        <paper-icon-button 
+                            id="prev" 
+                            @tap="${this.prev}"
+                            icon="chevron-left"
+
+                        ></paper-icon-button>
+                    `
+                }
+
+                ${ items.map(e => {
+                    return html`
+                        <paper-button
+                            @tap="${(ev) => this.select(ev, e)}"
+                            class="${classMap({
+                                active: this.index == e.page
+                            })}"
+                        >${e.label}</paper-button>
+                    `;
+                })}
+
+                ${ !this.hiddenNextPageButton &&
+                    html`
+                        <paper-icon-button 
+                            id="next" 
+                            @tap="${this.next}"
+                            icon="chevron-right" 
+                        ></paper-icon-button>
+                    `
+                }
+                ${ !this.hiddenLastPageButton &&
+                    html`
+                        <paper-icon-button 
+                            id="last" 
+                            @tap="${this.last}"
+                            icon="last-page" 
+                        ></paper-icon-button>
+                    `
+                }
+            </div>
+        `;
     }
 }
 
